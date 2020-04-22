7e12fb66ae5937d4da554b7ff2f42db8992965f5
ash37373 <ash37373@gmail.com>
2020-04-23 01:22:19 +0900

    2-1 할차례

---
+++ index.js
@@ -0,0 +1,41 @@
+// 첫사용은 const로 진짜 써야할때 let 쓰자
+// 여러줄 주석은 쉬프트 알트 에이
+//변수 데이터 타입
+
+/*String
+const what = "yujin";
+*/
+
+/* Boolean
+const wat = true; */
+
+/* Number
+const  wat = 666; */
+
+/* Float
+const wat = 55.1 */
+
+// Array
+/* const something = "Something";
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", something];
+console.log(daysOfWeek); */
+
+//Object 와 Array다른점은 Object에는 값에 이름을 줄수 있음
+/* const hwanInfo = {
+  name: "hwan",
+  age: 35,
+  gender: "Male",
+  isHansome: true,
+  favMovies: ["Along the gods", "Titanic", "Ninza"],
+  favFood: [
+    { name: "Kimchi", fatty: false },
+    { name: "Hamberger", fatty: false },
+  ],
+};
+console.log(hwanInfo);
+console.log(hwanInfo.name);
+hwanInfo.age = 38;
+console.log(hwanInfo.age);
+console.log(hwanInfo.favFood);
+console.log(hwanInfo.favFood[1]);
+console.log(hwanInfo.favFood[1].name); */
